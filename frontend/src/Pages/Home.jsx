ed09201dd9f4cf5a4d3ee60a72cbcf84082e8e65
SAMIVo007 <ys142051@gmail.com>
2024-04-20 22:29:07 +0530

    modified homepage, headerAdmin and dashboard

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,38 +1,96 @@
-@ -3,37 +3,31 @@ import HeaderHome from "../Components/HeaderHome";
+import HeaderHome from "../Components/HeaderHome";
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { loginUser } from "../utils/authAPI";
+import { isLogin, logOut } from "../utils/cookieSetup";
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/Anti-Corrupto.png";
 
 function Home() {
-  return (
-    <div className=" h-full ">
-      <HeaderHome/>
-      <div
-        className="h-[530px] flex flex-col items-center justify-center bg-cover "
-        style={{
-          backgroundImage: `URL(
+	const [isLoggedd, setisLoggedd] = useState(false);
+
+	const navigate = useNavigate();
+	useEffect(() => {
+		const checkLoginSession = isLogin();
+		if (checkLoginSession) {
+			setisLoggedd(true);
+		} else {
+			setisLoggedd(false);
+		}
+	}, []);
+
+	const handleLogout = () => {
+		logOut();
+		setisLoggedd(false);
+		navigate("/login");
+	};
+
+	return (
+		<div className=" h-screen ">
+			<div className="p-3 bg-slate-300 w-screen opacity-95 flex flex-row justify-between items-center">
+				{/* <div className=" h-1/6 bg-slate-800 "></div> */}
+
+				{/* <div className="p-4 flex flex-row justify-between items-center"> */}
+				<div className="flex flex-row text-gray-700 justify-center items-center rounded-lg font-bold hover:text-gray-900 ">
+					<img src={logo} alt="Image" className="h-12" />
+					<NavLink to={"/"}>Anti-Corruptō</NavLink>
+				</div>
+
+				<ul className="px-4 flex flex-row justify-end space-x-8  items-center">
+					<li className=" hover:text-blue-600">
+						<button>About</button>
+					</li>
+					<li className=" hover:text-blue-600">
+						<NavLink to={"/dashboard"}>
+							<button>Features</button>
+						</NavLink>
+					</li>
+					<li className=" hover:text-blue-600 ">
+						<button>Services</button>
+					</li>
+					<li className=" hover:text-blue-600">
+						<button>Contact</button>
+					</li>
+					{/* <div className="border px-8  border-slate-300  bg-slate-800 text-white text-sm rounded-3xl p-2 hover:bg-gray-900">
+							<NavLink to={"/login"}>Login</NavLink>
+						</div> */}
+				</ul>
+				{/* {isLoggedd ? (
+						<div className="border px-5  border-slate-300  bg-slate-800 text-white text-sm rounded-lg p-2 hover:bg-gray-900">
+							<button onClick={() => handleLogout()}>Logout</button>
+						</div>
+					) : (
+						<div className="border px-5  border-slate-300  bg-slate-800 text-white text-sm rounded-lg p-2 hover:bg-gray-900">
+							<NavLink to={"/login"}>Login</NavLink>
+						</div>
+					)} */}
+				{/* </div> */}
+			</div>
+			<div
+				className="h-[780px] p-20 flex flex-col items-start justify-center bg-cover "
+				style={{
+					backgroundImage: `URL(
             "https://images.unsplash.com/photo-1582647509711-c8aa8a8bda71?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           )`,
-        }}
-      >
-        <ul className=" flex flex-col justify-center items-center ">
-          <li className=" text-4xl font">LANDCHAIN</li>
-          <li className=" font-semibold p-10 text-bold">
-            "Blockchain technology has the potential to revolutionize land
-            registry systems by providing a secure, transparent, and immutable
-            record of property ownership. Through blockchain, land registry
-            processes can become more efficient, reducing fraud, disputes, and
-            bureaucratic hurdles. With its decentralized nature, blockchain
-            ensures trust among parties and creates a tamper-proof ledger,
-            making it a promising solution for modernizing and enhancing land
-            registry systems worldwide."
-          </li>
-        </ul>
-        <div>
-          <button className=" bg-slate-800 h-16 w-60 rounded-xl font-bold text-lg text-slate-100 hover:bg-black hover:text-amber-100 hover:underline">
-            Inspect Land
-          </button>
-        </div>
-      </div>
-      <div className=""></div>
-    </div>
-  );
+				}}
+			>
+				<ul className=" flex flex-col justify-center items-start ">
+					<li className=" text-8xl font-bold">Welcome.</li>
+					<li className=" font-semibold p-4 text-bold">
+						Transforming Land Registry with Blockchain: Secure, Transparent, Immutable
+						Ownership Records
+					</li>
+				</ul>
+				<div className="space-x-6 flex flex-row">
+					<div className="ml-2 bg-slate-800 p-2 px-12 rounded-3xl font-bold text-md text-slate-100 hover:bg-black hover:text-amber-100">
+						<NavLink to={"/signup"}>Sign In</NavLink>
+					</div>
+					<div className="ml-2 bg-slate-800 p-2 px-12 rounded-3xl font-bold text-md text-slate-100 hover:bg-black hover:text-amber-100">
+						<NavLink to={"/login"}>Login</NavLink>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 export default Home;
