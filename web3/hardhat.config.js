808b52d17bdabcffbb8b6af44b1711389d2ca4c6
Vishav Singla <95279293+vishavsingla@users.noreply.github.com>
2024-04-21 01:30:31 +0530

    ewsrt

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/MXvSnC_DxLZPdMJ-J39F7B78lBAdxNg4",
-      accounts: ["b7c9c339b65dcca2a05c9bc0a4b6943860f9f16a3a4ce6174ca29354bc84f186"],
+      url: "",
+      accounts: [""],
     },
   },
 };
