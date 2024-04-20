426fe5b73674493b39eec177ae32c0f067d6e9df
vishavsingla <vishav9933@gmail.com>
2024-04-21 04:38:05 +0530

    ...

--- frontend/src/utils/Web3/constants.js
+++
@@ -1,5 +0,0 @@
-import abi from "../LandRegisteryabi.json";
-
-// export const landAddress = "0xE91d86E5CFF2bf94d463A3D671BF4Ea67bDDF494"; //sepolia
-export const landAddress = "0x22B0D1351e1d0125B3b28d15C1C9D7fBa87a39d1"; //sepolia
-export const landABI = abi.abi;
