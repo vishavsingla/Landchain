39942eb5e790f7bc0826dac4954868bfea161d86
vishavsingla <vishav9933@gmail.com>
2024-04-20 19:40:28 +0530

    deployed contract again giving errors

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./LandRegisteryabi.json";
 
-export const landAddress = "0x48eC7847B826f4f4B7e275Cc62CF792199A48BE6"; //sepolia
+export const landAddress = "0xE91d86E5CFF2bf94d463A3D671BF4Ea67bDDF494"; //sepolia
 export const landABI = abi.abi;
--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -4,8 +4,9 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     sepolia: {
-      url: "#",
-      accounts: ["#"],
+      url: "",
+      accounts: [""],
     },
   },
 };
+
